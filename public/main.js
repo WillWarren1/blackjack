56a2255e086a67b14e25418242841ea0877cc9d0
Will Warren <willwarrenmail@gmail.com>
2019-02-09 11:54:00 -0500

    added weight to cards, created player hand

--- public/main.js
+++ public/main.js
@@ -1,7 +1,94 @@
+let valueOfCard = [
+  'ace',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  'jack',
+  'queen',
+  'king'
+]
+
+let suitOfCard = ['spades', 'hearts', 'clubs', 'diamonds']
+
+let deckOfCards = []
+
+let playerHand = []
+
+const buildDeck = () => {
+  for (let cardValue = 0; cardValue < valueOfCard.length; cardValue++) {
+    console.log(cardValue)
+    for (let cardSuit = 0; cardSuit < suitOfCard.length; cardSuit++) {
+      console.log(cardSuit)
+
+      let weight = parseInt(valueOfCard[cardValue])
+      if (
+        valueOfCard[cardValue] == 'jack' ||
+        valueOfCard[cardValue] == 'queen' ||
+        valueOfCard[cardValue] == 'king'
+      ) {
+        weight = 10
+      }
+      if (valueOfCard[cardValue] == 'ace') {
+        weight = 11
+      }
+      let name = valueOfCard[cardValue] + ' of ' + suitOfCard[cardSuit]
+
+      let card = {
+        Value: valueOfCard[cardValue],
+        Suit: suitOfCard[cardSuit],
+        Weight: weight,
+        Name: name
+      }
+
+      console.log(card)
+
+      deckOfCards.push(card)
+    }
+  }
+  console.log(deckOfCards)
+}
+
+const shuffleDeck = () => {
+  for (let i = 51; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+
+    let posI = deckOfCards[i]
+    let posJ = deckOfCards[j]
+
+    deckOfCards[j] = posI
+    deckOfCards[i] = posJ
+  }
+  console.log(deckOfCards)
+}
+
 const main = () => {
   if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+    document.querySelector('h1.hello-world').textContent =
+      "Let's play Blackjack!"
+  }
+  buildDeck()
+  shuffleDeck()
+}
+
+const drawCards = () => {
+  let newCard = deckOfCards.shift(0, 1)
+
+  console.log(deckOfCards)
+  if (deckOfCards.length > 0) {
+    const _li = document.createElement('li')
+    _li.textContent = newCard.Name
+    document.querySelector('ul').appendChild(_li)
+
+    playerHand.push(newCard)
+    console.log(playerHand)
   }
 }
 
 document.addEventListener('DOMContentLoaded', main)
+document.querySelector('#draw').addEventListener('click', drawCards)
